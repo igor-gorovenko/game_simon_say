e339b76c56671ba6c7527e26024fd3600d9fb98d
Igor Gorovenko <igorforspeak@gmail.com>
2021-04-28 21:42:27 +0300

    create new project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="app">
+		<GameSimonSay />
+	</div>
+</template>
+
+<script>
+import GameSimonSay from './components/GameSimonSay.vue'
+
+export default {
+	name: 'App',
+	components: {
+		GameSimonSay,
+	},
+}
+</script>
+
+<style></style>
---
+++ src/components/GameSimonSay.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		Simon Says
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'GameSimonSay',
+	props: {
+		msg: String,
+	},
+}
+</script>
+
+<style scoped></style>
